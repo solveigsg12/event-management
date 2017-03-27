3f1b638b66a52642bc8c1fec5b5e96b412ab4988
Eva Dögg Steingrímsdóttir <evasteingrims@gmail.com>
2017-03-27 18:59:34 +0000

    Test sendblue

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var sendinblue = require('sendinblue-api');
+
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,8 +16,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.onClick}>Test</button>
       </div>
     );
+  }
+
+  onClick() {
+    console.log("Click");
+    var parameters = { "apiKey": "AJFGsPLMhzdBq3IN", "timeout": 5000 }; //Optional parameter: Timeout in MS
+    var sendinObj = new sendinblue(parameters);
+
+    var input = {};
+    sendinObj.get_account(input, function(err, response){
+         console.log(response);
+    })
   }
 }
 
