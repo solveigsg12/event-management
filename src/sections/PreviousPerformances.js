8db107ddc608b3ca1184da77dfa5b6d88789e548
Solveig Sif <solveigsg12@ru.is>
2017-05-01 14:06:05 +0000

    previous performances styling

--- src/sections/PreviousPerformances.js
+++ src/sections/PreviousPerformances.js
@@ -4,8 +4,13 @@ import './PreviousPerformances.css';
 class PreviousPerformances extends Component {
   render() {
     return (
-      <div className="Contact">
-        <h1>Previous Performances</h1>
+      <div className="perviousPerformancesContainer">
+        <div className="previousPerformancesTitle">Previous Performances</div>
+        <div className="performancesImageContainer">
+          <div className="performance1">The Car</div>
+          <div className="performance2">The Ocean</div>
+          <div className="performance3">The mountain</div>
+        </div>
       </div>
     );
   }
