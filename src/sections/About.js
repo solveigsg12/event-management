eb3e5f684ab82710c4d2ae134319b046caa5ce22
Solveig Sif <solveigsg12@ru.is>
2017-05-01 22:28:55 +0000

    styling and header added to contact and about

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Header from './Header';
 import './About.css';
 
 class AboutUs extends Component {
   render() {
     return (
       <div className="Contact">
+        <Header />
         <h1>About Us</h1>
       </div>
     );
