8958c7bd698d3c0661bf990afee4315b06e0f080
Solveig Sif <solveigsg12@ru.is>
2017-05-16 21:14:30 +0000

    merge from master and we were on master when we were adding background video and fixing previousperformances info

--- src/actions/previousPerformances.js
+++ src/actions/previousPerformances.js
@@ -1,7 +1,6 @@
 import * as actionTypes from './actionTypes';
 
 export const setPreviousPerformance = name => {
-  console.log(name, ' hvað er name i actions??');
   return {
     type: actionTypes.SET_PREVIOUS_PERFORMANCE,
     name,
